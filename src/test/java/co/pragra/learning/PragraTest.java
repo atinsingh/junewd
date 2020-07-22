8d714fb5ca6ba0445ecbbebe74287e1912296b3d
Atin Singh <atin.singh@gmail.com>
2020-07-22 11:35:20 -0400

    Update PragraTest.java

--- src/test/java/co/pragra/learning/PragraTest.java
+++ src/test/java/co/pragra/learning/PragraTest.java
@@ -1,6 +1,10 @@
 package co.pragra.learning;
 
 public class PragraTest {
+  // THis is remote
+  // I am adding this on remote
+  // I want to delete this
   public void helloTest(){
     System.out.println("All GOOd");
+  }
 }
