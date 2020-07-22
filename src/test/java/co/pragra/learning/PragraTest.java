2adf6337558589dd7ebc47156d97803839a8fe97
Baljinder <baljinderk274@gmail.com>
2020-07-22 11:30:14 -0400

    Added a Statement

--- src/test/java/co/pragra/learning/PragraTest.java
+++ src/test/java/co/pragra/learning/PragraTest.java
@@ -1,4 +1,10 @@
 package co.pragra.learning;
 
 public class PragraTest {
-}
+    public static void main(String[] args) {
+
+            System.out.println("Print this statement");
+        }
+
+
+    }
