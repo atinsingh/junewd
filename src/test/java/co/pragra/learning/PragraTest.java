4c30278c78952509941fbfb20591f826ca0fb612
aayush <dhiraayush@ymail.com>
2020-07-22 12:01:43 -0400

    changes

--- src/test/java/co/pragra/learning/PragraTest.java
+++ src/test/java/co/pragra/learning/PragraTest.java
@@ -2,11 +2,12 @@ package co.pragra.learning;
 
 import org.w3c.dom.ls.LSOutput;
 
+// this is not code
 public class PragraTest {
-
+// this is a practice
     public static void main(String[] args) {
 
-
+        System.out.println("do not print anyline");
         System.out.println("Hello Atin !!");
     }
 }
--- src/test/java/co/pragra/learning/learning_123.java
+++ src/test/java/co/pragra/learning/learning_123.java
@@ -1,4 +1,9 @@
 package co.pragra.learning;
 
 public class learning_123 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
+
