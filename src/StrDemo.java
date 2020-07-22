55fed0058cebaab0e963180dfc0a4591766d9d8a
Rushil <rushil0923@gmail.com>
2020-07-22 11:45:01 -0400

    hello '

---
+++ src/StrDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Arrays.*
+
+public class StrDemo {
+
+    int [][] arr = new int[2][3];
+    for(int i=0; i<arr.length;i++){
+
+    }
+
+
+
+
+}
--- src/test/java/co/pragra/learning/PragraTest.java
+++ src/test/java/co/pragra/learning/PragraTest.java
@@ -1,4 +1,11 @@
 package co.pragra.learning;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class PragraTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Atin !!");
+    }
 }
