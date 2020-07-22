580284a97f943bc14de68f5249655de6f82179c5
Singh,Atin <atin@pragra.co>
2020-07-22 12:52:54 -0400

    Udpaed with webElement

--- src/test/java/co/pragra/learning/HomeTest.java
+++ src/test/java/co/pragra/learning/HomeTest.java
@@ -3,87 +3,88 @@ package co.pragra.learning;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class HomeTest {
 
+    WebDriver driver;
+
     @BeforeSuite
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "/Users/atinsingh/Downloads/chromedriver");
+        driver  = new ChromeDriver();
+        driver.get("https://www.skipthedishes.com/");
     }
 
     @Test
     public void openPragraWeb() throws InterruptedException, MalformedURLException {
-        WebDriver driver = new ChromeDriver(); // this line will create a broswer instance
-       driver.get("https://zoom.us");
 
-       driver.manage().timeouts().setScriptTimeout(1, TimeUnit.MILLISECONDS);
-       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-
-        ((JavascriptExecutor) driver).executeScript("console.log('Learning for good')");
-
-        driver.findElement(By.id("truste-consent-button")).click();
-//
-//        WebElement elm = driver.findElement(By.xpath("//div[@id='navbar']//a[contains(@class,'top-contactsales')]"));
-////        Actions actions = new Actions(driver);
-////        actions.keyDown(Keys.COMMAND).pause(1000).click(elm).build().perform();
-//        elm.click();
-//        Thread.sleep(3000);
-//        driver.navigate().back();
-//        Thread.sleep(3000);
-//        driver.navigate().forward();
-//        Thread.sleep(3000);
-//        driver.navigate().refresh();
-
-
-
-
-//         Set<String> windows =  driver.getWindowHandles();
-//
-//         String parent = driver.getWindowHandle();
-
-//         for (String window: windows) {
-//             if(!window.equals(parent)){
-//                 driver.switchTo().window(window);
-//                 break;
-//             }
-//         }
-
-
-
-//         driver.findElement(By.cssSelector("#email")).sendKeys("AYUSH");
-//         Thread.sleep(3000);
-//
-//
-//         driver.switchTo().window(parent);
-//
-//        ((JavascriptExecutor) driver).executeScript("confirm('Fake Alert')");
-
-
-
-
-        Thread.sleep(10000);
-
-        System.out.println(driver.getCurrentUrl());
-        System.out.println(driver.getTitle());
-        driver.quit();
     }
 
     @Test
-    public void dummyTest(){
+    public void dummyTest() throws InterruptedException {
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+            //Locaters allow us to locate elemt
+            // id  -- locate by ID
+            // tagName -- locate by tagName
+            // cssSelector
+            // Xpath
+            // linkText
+            // partialLinkTest
+           // WebElement element = driver.findElement(By.xpath("//form/input"));
+
+    //        WebElement icon = driver.findElement(By.cssSelector(".c5159"));
+    //        icon.click();
+
+            WebElement form = driver.findElement(By.cssSelector("form>input"));
+            Thread.sleep(2000);
+    //        ((JavascriptExecutor) driver).executeScript("arguments[0].style.display='none'", form);
+            //((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('disabled','disable')", form);
+
+            Thread.sleep(2000);
+
+             System.out.println( form.getAttribute("placeholder"));
+        System.out.println(form.getCssValue("color"));
+        System.out.println(form.getRect().height);
+        System.out.println(form.getRect().width);
+
+            form.sendKeys("200", " Matheson Blvd" , Keys.ENTER);
+
+            File file = form.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.copy(file.toPath(), Paths.get("Screenshot.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        //System.out.println(driver.findElement(By.cssSelector("h1.MuiTypography-root ")).getText());
+
+            Thread.sleep(10000);
+
+           // element.click();
 
 
     }
     @Test
     public void greeting(){
 
+    }
+
+    @AfterSuite
+    public void quit(){
+        driver.quit();
     }
 }
 
